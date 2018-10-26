fdb4f2badb3e48dc152f12813143d90602c98670
Alvin <alvintu@Alvins-MacBook-Pro.local>
2018-10-26 17:03:04 -0400

    Initial Commit

---
+++ MealDB-dot-com/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MealDB-dot-com
+//
+//  Created by Alvin on 10/26/18.
+//  Copyright © 2018 AlvinTu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
